776ecd8f4e2d53605e2bac9cd6c6e235676a68c7
ddnomad <dd@ddworks.io>
2019-12-09 03:35:23 +0200

    Added all currently planned root cmd options

--- zgcli/config.py
+++ zgcli/config.py
@@ -1,5 +1,4 @@
 """."""
-import logging
 import os
 import platform
 
@@ -8,17 +7,48 @@ from box import Box, BoxKeyError
 
 DEFAULTS = {
     'display': {
-        'max_content_width': 120
+        'max_content_width': 120,
+        'format': 'verbose',
+        'acceptable_formats': [
+            'csv',
+            'json',
+            'tree',
+            'verbose',
+            'yaml'
+        ]
     },
 
     'internal': {
-        'default_user_config_path': '~/.config/zg/config.yml',
-        'default_user_config_path_darwin': '~/config/zg/config.yml',
-        'default_user_config_path_linux': '~/config/zg/config.yml',
-        'default_user_config_path_windows': '%USERPROFILE%/.zg/config.yml',
+        'user_config_path': '~/.config/zg/config.yml',
+        'user_config_path_darwin': '~/.config/zg/config.yml',
+        'user_config_path_linux': '~/.config/zg/config.yml',
+        'user_config_path_windows': '%USERPROFILE%/.zg/config.yml',
 
-        'default_log_format': 'native',
-        'acceptable_log_formats': ['native', 'jsonl', 'syslog']
+        'user_identity_path': '~/.config/zg/identity',
+        'user_identity_path_darwin': '~/.config/zg/identity',
+        'user_identity_path_linux': '~/.config/zg/identity',
+        'user_identity_path_windows': '%USERPROFILE%/.zg/identity'
+    },
+
+    'logging': {
+        'log_format': 'native',
+        'acceptable_log_formats': [
+            'native',
+            'jsonl',
+            'syslog'
+        ],
+
+        'log_level': 'info',
+        'acceptable_log_levels': [
+            'debug',
+            'dbg',
+            'info',
+            'inf',
+            'warning',
+            'warn',
+            'critical',
+            'crit'
+        ],
     }
 }
 
@@ -33,16 +63,30 @@ class Config(Box):
         """."""
         return Config(DEFAULTS)
 
-    def get_default_user_config_path(self):
-        """Get expanded and OS-specific default user configuration path.
+    def get_user_config_path(self):
+        """Get expanded and OS-specific path to user configuration file.
 
-        :return: Default user configuration file path specific to current
+        :return: Path to a user configuration file specific to current
                  operating system user and system type.
         :rtype:  str
         """
         try:
-            path = self.internal['default_user_config_path_%s' % self.PLATFORM]
+            path = self.internal['user_config_path_%s' % self.PLATFORM]
         except BoxKeyError:
-            path = self.internal.default_user_config_path
+            path = self.internal.user_config_path
+
+        return os.path.expanduser(path)
+
+    def get_user_identity_path(self):
+        """Get expanded and OS-specific path to user identity file.
+
+        :return: Path to user identity file path specific to current operating
+                 system user and system type.
+        :rtype:  str
+        """
+        try:
+            path = self.internal['user_identity_path_%s' % self.PLATFORM]
+        except BoxKeyError:
+            path = self.internal.user_identity_path
 
         return os.path.expanduser(path)
--- zgcli/main.py
+++ zgcli/main.py
@@ -1,6 +1,7 @@
 """Root ZeroGuard CLI command."""
-import click
 import sys
+
+import click
 
 from zgcli import DEFAULT_CONFIG
 from zgcli.commands import ENABLED_COMMANDS
@@ -15,7 +16,25 @@ CONTEXT_SETTINGS = {
 
 OPT_CONFIG_KWARGS = {
     'help': 'Path to a configuration file.',
-    'default': DEFAULT_CONFIG.get_default_user_config_path(),
+    'default': DEFAULT_CONFIG.get_user_config_path(),
+    'metavar': 'PATH',
+    'show_default': True,
+    'type': click.Path(exists=False)
+}
+
+
+OPT_FORMAT_KWARGS = {
+    'help': 'Data output format.',
+    'default': DEFAULT_CONFIG.display.format,
+    'metavar': 'FORMAT',
+    'show_default': True,
+    'type': click.Choice(DEFAULT_CONFIG.display.acceptable_formats)
+}
+
+
+OPT_IDENTITY_KWARGS = {
+    'help': 'Path to an identity file.',
+    'default': DEFAULT_CONFIG.get_user_identity_path(),
     'metavar': 'PATH',
     'show_default': True,
     'type': click.Path(exists=False)
@@ -31,10 +50,19 @@ OPT_LOG_KWARGS = {
 
 OPT_LOG_FORMAT_KWARGS = {
     'help': 'Logging format to use if logging is enabled.',
-    'default': DEFAULT_CONFIG.internal.default_log_format,
+    'default': DEFAULT_CONFIG.logging.log_format,
     'metavar': 'FORMAT',
     'show_default': True,
-    'type': click.Choice(DEFAULT_CONFIG.internal.acceptable_log_formats)
+    'type': click.Choice(DEFAULT_CONFIG.logging.acceptable_log_formats)
+}
+
+
+OPT_LOG_LEVEL_KWARGS = {
+    'help': 'Logging level to set if logging is enabled.',
+    'default': DEFAULT_CONFIG.logging.log_level,
+    'metavar': 'LEVEL',
+    'show_default': True,
+    'type': click.Choice(DEFAULT_CONFIG.logging.acceptable_log_levels)
 }
 
 
@@ -68,8 +96,11 @@ OPT_VERSION_KWARGS = {
 
 @click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
 @click.option('-c', '--config', **OPT_CONFIG_KWARGS)
+@click.option('-f', '--format', **OPT_FORMAT_KWARGS)
+@click.option('-i', '--identity', **OPT_IDENTITY_KWARGS)
 @click.option('-L', '--log', **OPT_LOG_KWARGS)
 @click.option('-F', '--log-format', **OPT_LOG_FORMAT_KWARGS)
+@click.option('-l', '--log-level', **OPT_LOG_LEVEL_KWARGS)
 @click.option('-S', '--log-stdout', **OPT_LOG_STDOUT_KWARGS)
 @click.option('-C', '--no-config', **OPT_NO_CONFIG_KWARGS)
 @click.option('-q', '--quiet', **OPT_QUIET_KWARGS)
