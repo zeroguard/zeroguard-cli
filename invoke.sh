abc2746fdfa557edaf038bad5aa54af230ba7bb5
Dubbinary <dubbinary@gmail.com>
2019-12-15 00:56:30 +0200

    Added invoke script for running the app

---
+++ invoke.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+###############################################################################
+# Title:       invoke.sh
+# Description: Wrapper around zg CLI application to run it without installing
+#              as a package or generating a binary. Convenience script only and
+#              should not be a way to get zg running on user's host.
+# Usage:       invoke.sh ZG_CLI_OPTIONS_HERE
+set -euo pipefail
+python3 -m zgcli.main "$@"
